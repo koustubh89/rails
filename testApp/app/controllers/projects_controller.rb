7f1fa613d0b6ddc38bc514fbc4b16b41aac5e4b9
koustubh89 <koustubh89@gmail.com>
2013-10-29 12:45:37 +0530

    Update projects_controller.rb

--- testApp/app/controllers/projects_controller.rb
+++ testApp/app/controllers/projects_controller.rb
@@ -4,8 +4,8 @@ class ProjectsController < ApplicationController
 
   def index
     @projects = Project.order(:name)
-   @projects = Project.paginate page: params[:page],
-      per_page: 6
+    @projects = Project.paginate page: params[:page],
+    per_page: 6
   
     respond_to do |format|
       format.html # index.html.erb
@@ -44,29 +44,29 @@ class ProjectsController < ApplicationController
   # POST /projects.json
   def create
     @project = Project.new(params[:project])
-                flag = 0
-		@user = User.all
-		@user.each do |user|
+    	flag = 0
+	@user = User.all
+	@user.each do |user|
 		    if (user.id == @project.user_id)
 			flag = 1
 			break
 		    end
-	 	end
+	 end
 
-if flag==1
-    respond_to do |format|
-      if @project.save
-        format.html { redirect_to @project, notice: 'Project was successfully created.' }
-        format.json { render json: @project, status: :created, location: @project }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
-    end
-else
-        redirect_to new_project_path, notice: 'Emp-Id not available. Refer view all employees'
+    if flag==1
+         respond_to do |format|
+                    if @project.save
+        		format.html { redirect_to @project, notice: 'Project was successfully created.' }
+        		format.json { render json: @project, status: :created, location: @project }
+      		    else
+        		format.html { render action: "new" }
+        		format.json { render json: @project.errors, status: :unprocessable_entity }
+      		     end
+         end
+     else
+         redirect_to new_project_path, notice: 'Emp-Id not available. Refer view all employees'
         
-end
+     end
 
   end
 
@@ -81,61 +81,61 @@ end
       #pro_id=@user.project_id
       #render text: pro_name
 
-    respond_to do |format|
-      t=1
-      if @project.update_attributes(params[:project])
-         if(@user.project_name=="none")				
+         respond_to do |format|
+                    t=1
+            if @project.update_attributes(params[:project])
+                    if(@user.project_name=="none")				
 	
  
-        @user.update_attribute(:project_name,@project.name)
-	@user.update_attribute(:project_id,@project.id)
+                    	@user.update_attribute(:project_name,@project.name)
+			@user.update_attribute(:project_id,@project.id)
 
-         else
-	arr=Array.new
-	arr=pro_name.split(", ")
+         	     else
+			arr=Array.new
+			arr=pro_name.split(", ")
 
-	arr.each do |i|
-                 if(i == @project.name)
-			@user.update_attribute(:project_name,pro_name)
-			@user.update_attribute(:project_id,pro_id)
-			t=0
-			break
+			arr.each do |i|
+                 		if(i == @project.name)
+				@user.update_attribute(:project_name,pro_name)
+				@user.update_attribute(:project_id,pro_id)
+				t=0
+				break
                           
-	         end
-	  end #end of aaray
-         if t==1		
+	        		 end
+	  		end #end of aaray
+         	    if t==1		
 
-			    @user.update_attribute(:project_name,pro_name + ", " + @project.name)
-         end		
-        end
+			 @user.update_attribute(:project_name,pro_name + ", " + @project.name)
+         	     end		
+             end
 	
+		
+       		     format.html { redirect_to @project, notice: 'Project was successfully updated.' }
+		     format.json { head :no_content }
 
-       format.html { redirect_to @project, notice: 'Project was successfully updated.' }
-        format.json { head :no_content }
-
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @project.errors, status: :unprocessable_entity }
-      end
+             else
+        	     format.html { render action: "edit" }
+                     format.json { render json: @project.errors, status: :unprocessable_entity }
+             end
     
     end
 
 	
 	#render text: @user.id
 
-#changes done here... repeated code removed
+  #changes done here... repeated code removed
 
-end
+  end
 
   # DELETE /projects/1
   # DELETE /projects/1.json
   def destroy
-    @project = Project.find(params[:id])
-    @project.destroy
+	       @project = Project.find(params[:id])
+	       @project.destroy
 
     respond_to do |format|
-      format.html { redirect_to projects_url }
-      format.json { head :no_content }
+                format.html { redirect_to projects_url }
+                format.json { head :no_content }
     end
   end
 
